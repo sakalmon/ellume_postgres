c8778cddd7188958ee7eec7c728678be9f145d40
Sakal Mon <sakalathmon@gmail.com>
2021-11-04 19:49:58 +1000

    Create table and store all results

--- main.py
+++ main.py
@@ -44,39 +44,8 @@ cur = conn.cursor()
 
 engine = create_engine(f'postgresql://postgres:{POSTGRES_PASSWORD}@localhost:5432/mydb')
 
-# for file in os.listdir(CSV_DIR):
-#     if file.startswith('6171-ASM') and file.endswith(CEQ_MAP['AR2']['Line1'] + '.csv'):
-    
-#         df = pd.read_csv(os.path.join(CSV_DIR, file), header=2)
-
-#         columns = list(df.columns)
-
-#         for column in columns:
-#             matches = pattern.findall(column)
-                
-#             if matches:
-#                 temp = []
-#                 temp.append(column.replace(matches[0], '_'))
-
-#                 for match in matches:
-#                     temp.append(temp.pop().replace(match, '_'))
-
-#                 sql = ('ALTER TABLE ceq0178 '
-#                         f'ADD COLUMN {temp[0]} VARCHAR(50);')
-#                 cur.execute(sql)
-
-#             else:
-#                 sql = ('ALTER TABLE ceq0178 '
-#                         f'ADD COLUMN {column} VARCHAR(50);')
-#                 cur.execute(sql)
-        
-#         conn.commit()
-
-# cur.close()
-# conn.close()
-
 for file in os.listdir(CSV_DIR):
-    if file.startswith('6171-ASM') and file.endswith(CEQ_MAP['AR2']['Line1'] + '.csv'):
+    if file.startswith('6171-ASM') and file.endswith('.csv'):
         renamed_columns = []
 
         df = pd.read_csv(os.path.join(CSV_DIR, file), header=2)
@@ -86,24 +55,28 @@ for file in os.listdir(CSV_DIR):
         for column in columns:
             matches = pattern.findall(column)
                 
-            if matches:
-                temp = []
-                temp.append(column.replace(matches[0], '_'))
+            # if matches:
+            #     temp = []
+            #     temp.append(column.replace(matches[0], '_'))
 
-                for match in matches:
-                    temp.append(temp.pop().replace(match, '_'))
+            #     for match in matches:
+            #         temp.append(temp.pop().replace(match, '_'))
                 
-                renamed_columns.append(temp[0])
+            #     renamed_columns.append(temp[0])
             
-            else:
-                renamed_columns.append(column)
+            # else:
+            renamed_columns.append(column)
 
-        renamed_columns = ', '.join(renamed_columns)
-        sql = (f'INSERT INTO ceq0178({renamed_columns}) '
-                'VALUES (", ".join(df.iloc[0].values.tolist()));')
+        renamed_columns = ' VARCHAR(50), '.join(renamed_columns) + ' VARCHAR(50)'
+        table = file.replace('-', '_')
+        table = table[:-4]
+        # sql = (f'CREATE TABLE "{table}"({renamed_columns});')
 
-        cur.execute(sql)
-        conn.commit()
+        # cur.execute(sql)
+        # conn.commit()
+        # cur.close()
+        # conn.close()
+        print(f'Adding to table: {table}')
+        df.to_sql(table, engine, if_exists='append', index=False)
+        
 
-cur.close()
-conn.close()
